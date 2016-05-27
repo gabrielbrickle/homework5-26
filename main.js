a58c38fbda983d12a427ec98ade7ddd1e3e0d39f
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-05-27 09:03:57 -0400

    done

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // code goes here :)
 
 //1. What is the average price of all items?///
-function ans1 () {
 var listOfPrices = items.map(function(element,idx,arr){
   return element.price
 });
@@ -12,12 +11,12 @@ listOfPrices.forEach(function(element,idx,arr) {
 });
 
 var avg = sum/listOfPrices.length
-}
+
+document.getElementById('answer1').innerHTML = avg;
 
 //2. Show me an array of items that cost between $14.00 and $18.00 USD?
 
-// document.getElementsByClassName('answer2')
-function ans2 () {
+
 
 var price14to18 = items.filter(function(element,idx,arr) {
   return element.price >= 14.00 && element.price <= 18.00 && element.currency_code === "USD"
@@ -25,32 +24,35 @@ var price14to18 = items.filter(function(element,idx,arr) {
   return element.title
 });
 
-}
+
+document.getElementById('answer2').innerHTML = price14to18;
+
+
 //3. Which item has a "GBP" currency code? Display it's name and price.
 
-function ans3 () {
 var gbpCurrency = items.filter(function(element,idx,arr) {
   return element.currency_code === "GBP"
 }).map (function(element,idx,arr) {
   return element.title + " " + element.price
 });
 
-}
+
+document.getElementById('answer3').innerHTML = gbpCurrency;
+
+
 //4. Display a list of all items who are made of wood.
 
-function ans4 {
 var listOfItems = items.filter(function(element,idx,arr) {
   return element.materials.indexOf("wood") !==-1
 }).map(function(element,idx,arr) {
  return element.title
 });
 
-}
 
+document.getElementById('answer4').innerHTML = listOfItems;
 
 //5. Which items are made of eight or more materials? Display the name, number of items and the items it is made of.
 
-function ans5 {
 
 var eightMaterials = items.filter(function(element,idx,arr) {
   return element.materials <= 8
@@ -58,14 +60,14 @@ var eightMaterials = items.filter(function(element,idx,arr) {
   return element.quantity + element.materials + element.title
 });
 
-}
 
-//6. How many items were made by their sellers?
+document.getElementById('answer5').innerHTML = eightMaterials;
 
-function ans6 {
+// 6. How many items were made by their sellers?
+
 var homeMade = items.filter(function(element,idx,arr){
   return element.who_made === "i_did"
 });
-homeMade.length
 
-}
+
+document.getElementById('answer6').innerHTML = homeMade.length;
